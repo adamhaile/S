a222ab33ebeeea6e771ad311ccd2ce389d6e97d9
Adam Haile <aah@curveship.org>
2014-05-07 17:50:38 -0400

    Revamping XO.js to not use global array of edges and updaters

--- js/XO.js
+++ js/XO.js
@@ -2,15 +2,14 @@ var X = (function () {
     "use strict";
 
     var count = 1,
-        consumer = 0,
-        edges = [],
-        updaters = [],
-        bundler = noop;
+        linker = undefined,
+        bundler = undefined;
 
     // initializer
     X.lift     = lift;
 
     X.val      = val;
+    X.ch       = ch;
     X.proc     = proc;
     X.bundle   = bundle;
     X.peek     = peek;
@@ -22,20 +21,15 @@ var X = (function () {
     }
 
     function lift(arg1, arg2) {
-        return typeof arg1 === 'function' ? proc(arg1, arg2)
+        return arguments.length == 0 ? ch()
+            : typeof arg1 === 'function' ? proc(arg1, arg2)
             : arg1 instanceof Array ? X.seq(arg1)
             : val(arg1);
     }
 
     function val(value) {
         var id = count++,
-            propagate = propagateImmediately;
-
-        edges[id] = {};
-
-        val.out = out;
-
-        bundler(val);
+            updaters = {};
 
         return val;
 
@@ -43,114 +37,131 @@ var X = (function () {
             if (arguments.length > 0) {
                 if (value !== set_value) {
                     value = set_value;
-                    propagate(id);
+                    propagate(updaters);
                 }
             } else {
-                addEdge(id);
+                if (linker) linker(id, updaters);
             }
             return value;
         }
+    }
 
-        function out(mod) {
-            propagate = mod(propagate);
+    function ch(msg) {
+        var id = count++,
+            updaters = {};
 
-            return val;
+        return ch;
+
+        function ch(new_msg) {
+            if (arguments.length > 0) {
+                msg = new_msg;
+                propagate(updaters);
+            } else {
+                if (linker) linker(id, updaters);
+            }
+            return msg;
         }
     }
 
     function proc(get, set) {
         var id = count++,
-            propagate = propagateImmediately,
-            _bundler = bundler,
             updating = false,
-            value;
-
-        edges[id] = {};
-        updaters[id] = update;
+            value,
+            sources = {},
+            updaters = {},
+            our_bundler = bundler;
 
         proc.in = _in;
-        proc.out = out;
 
-        _bundler(proc);
+        if (our_bundler) our_bundler(proc);
 
         update();
 
         return proc;
 
         function proc(setValue) {
-            var _consumer,
-                __bundler;
+            var prev_linker,
+                prev_bundler;
 
             if (arguments.length > 0) {
                 if (set) {
-                    _consumer = consumer, consumer = 0;
-                    __bundler = bundler, bundler = _bundler;
+                    prev_linker = linker, linker = undefined;
+                    prev_bundler = bundler, bundler = our_bundler;
+
                     try {
                         set(setValue);
                     } finally {
-                        consumer = _consumer;
-                        bundler = __bundler;
+                        linker = prev_linker;
+                        bundler = prev_bundler;
                     }
                 }
             } else {
-                addEdge(id);
+                if (linker) linker(id, updaters);
             }
 
             return value;
         }
 
-        function _in(mod) {
-            updaters[id] = mod(updaters[id]);
-
-            return proc;
-        }
-
-        function out(mod) {
-            propagate = mod(propagate);
-
-            return proc;
-        }
-
         function update() {
             var newValue,
-                _consumer,
-                __bundler;
+                prev_linker,
+                prev_bundler;
 
             if (!updating) {
                 updating = true;
-                _consumer = consumer, consumer = id;
-                __bundler = bundler, bundler = _bundler;
+                prev_linker = linker, linker = our_linker;
+                prev_bundler  = bundler, bundler  = our_bundler;
 
                 try {
                     newValue = get();
                 } finally {
                     updating = false;
-                    consumer = _consumer;
-                    bundler = __bundler;
+                    linker = prev_linker;
+                    bundler = prev_bundler;
                 }
 
                 if (value !== newValue) {
                     value = newValue;
-                    propagate(id);
+                    propagate(updaters);
+                }
+            }
+        }
+
+        function _in(mod) {
+            update = mod(update, sources);
+
+            return proc;
+        }
+
+        function updateref() {
+            update();
+        }
+
+        function our_linker(sid, updaters) {
+            sources[sid] = updaters;
+            updaters[id] = updateref;
+        }
+
+        function unlink() {
+            var sid, updaters;
+
+            for (sid in sources) {
+                updaters = sources[sid];
+                if (updaters) {
+                    updaters[id] = undefined;
+                    sources[sid] = undefined;
                 }
             }
         }
     }
 
-    function addEdge(source) {
-        if (consumer) {
-            edges[source][consumer] = true;
-        }
-    }
+    function propagate(updaters) {
+        var id, updater;
 
-    function propagateImmediately(source) {
-        var consumers = edges[source],
-            consumer;
-
-        for (consumer in consumers) {
-            if (consumers[consumer]) {
-                consumers[consumer] = false;
-                updaters[consumer](consumer);
+        for (id in updaters) {
+            updater = updaters[id];
+            if (updater) {
+                updater();
             }
         }
     }
@@ -158,10 +169,8 @@ var X = (function () {
     function bundle(fn) {
         var nodes = [],
             inMods = identity,
-            outMods = identity,
             bundle = {
                 in: _in,
-                out: out,
                 watch: watch
             };
 
@@ -170,25 +179,23 @@ var X = (function () {
         return bundle;
 
         function watch(fn) {
-            var _bundler = bundler, bundler = add;
+            var prev_bundler = bundler, bundler = add;
 
             try {
                 fn();
             } finally {
-                bundler = _bundler;
+                bundler = prev_bundler;
             }
 
             function add(node) {
                 if (node.in) node.in(inMods);
-                if (node.out) node.out(outMods);
                 nodes.push(node);
                 _bundler(node);
             }
         }
 
         function _in(mod) {
-            var i,
-                node;
+            var i, node;
 
             inMods = compose(inMods, mod);
 
@@ -199,37 +206,23 @@ var X = (function () {
 
             return bundle;
         }
-
-        function out(mod) {
-            var i,
-                node;
-
-            outMods = compose(outMods, mod);
-
-            for (var i = 0; i < nodes.length; i++) {
-                node = nodes[i];
-                if (node.out) node.out(mod);
-            }
-
-            return bundle;
-        }
     }
 
     function peek(fn) {
-        var _consumer = consumer;
+        var prev_linker = linker;
 
-        consumer = 0;
+        linker = undefined;
 
         try {
             return fn();
         } finally {
-            consumer = _consumer;
+            linker = prev_linker;
         }
     }
 
     function compose(g, f) {
-        return function (x) {
-            return g(f(x));
+        return function (x, y) {
+            return g(f(x, y), y);
         };
     }
 
