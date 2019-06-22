df42ab73a85a5c2c2fafc93f472001fa5387ee9b
jeff@procata.com <jeff@procata.com>
2019-06-22 08:28:46 -0700

    Rebuild with new version of rollup and typescript

--- dist/S.js
+++ dist/S.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global.S = factory());
-}(this, (function () { 'use strict';
+    (global = global || self, global.S = factory());
+}(this, function () { 'use strict';
 
     // Public interface
     var S = function S(fn, value) {
@@ -539,4 +539,4 @@
 
     return S;
 
-})));
+}));
--- dist/withsubclocks.js
+++ dist/withsubclocks.js
@@ -1,8 +1,8 @@
 (function (global, factory) {
     typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
     typeof define === 'function' && define.amd ? define(factory) :
-    (global.S = factory());
-}(this, (function () { 'use strict';
+    (global = global || self, global.S = factory());
+}(this, function () { 'use strict';
 
     // Public interface
     var S = function S(fn, value) {
@@ -656,4 +656,4 @@
 
     return S;
 
-})));
+}));
